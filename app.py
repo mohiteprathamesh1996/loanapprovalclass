25cef846f878cb74a5051fe3a802e1b763dbb578
Prathamesh Mohite <59126968+mohiteprathamesh1996@users.noreply.github.com>
2023-01-19 17:11:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -16,15 +16,40 @@ def predict():
     For rendering results on HTML GUI
     '''
     int_features = [x for x in request.form.values()]
-    # prediction = model.predict(
-	  #   pd.DataFrame(int_features, columns=['Loan_ID', 'Gender', 'Married', 'Dependents', 'Education',
-    #    'Self_Employed', 'ApplicantIncome', 'CoapplicantIncome', 'LoanAmount',
-    #    'Loan_Amount_Term', 'Credit_History', 'Property_Area'])
-    # )
+    
+    rescap = pd.DataFrame(
+      int_features,
+      index=[
+        'Loan_ID', 'Gender', 'Married', 'Dependents', 'Education',
+        'Self_Employed', 'ApplicantIncome', 'CoapplicantIncome', 'LoanAmount',
+        'Loan_Amount_Term', 'Credit_History', 'Property_Area'
+        ]
+        ).T
 
-    output = int_features
+    rescap[['ApplicantIncome',
+     'CoapplicantIncome',
+     'LoanAmount',
+     'Loan_Amount_Term',
+     'Credit_History']] = rescap[['ApplicantIncome',
+     'CoapplicantIncome',
+     'LoanAmount',
+     'Loan_Amount_Term',
+     'Credit_History']].astype(float)
 
-    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+    output = model.predict(rescap)[0]
+
+    if output=="Y":
+      status = "Approved"
+    else:
+      status = "Rejected"
+
+    return render_template(
+      'index.html', 
+      prediction_text='Loan Appication Status : {}'.format(
+        status
+        )
+      )
+
 
 
 if __name__ == "__main__":
